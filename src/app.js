da020a4eb6eafa39ef0ea785e203b7c4c4845903
Sonika Singh <sonikasingh2401@gmail.com>
2025-01-09 18:25:21 +0530

    API level validation and Validator library function

--- src/app.js
+++ src/app.js
@@ -2,25 +2,85 @@ const express = require("express");
 const connectDB = require('./config.js/database');
 const app = express();
 const User = require('./models/user');
+const user = require("./models/user");
 
+app.use(express.json());
+
+//To save the data into database 
 app.post("/signup",async (req,res)=>{
-    const user = new User({
-        firstName:"Ursha",
-        lastName:"Kanol",
-        age:28,
-        gender:"female",
-        city:"Goa",
-        phoneNumber:9874745626,
-    });
+
+//Creating the new instance of the User model
+    const user = new User(req.body);
     try{
         await user.save();
         res.send("data saved successfull..");
+
     }
     catch(err){
         res.status(400).send("Data not saved.." +err.message)
     }
     
+ });
+
+//To Get the data from the database by Gender - male
+
+app.get("/user",async (req,res)=>{
+    const userGender = req.body.gender;
+
+    try{
+         const users = await User.find({gender: userGender})
+         if(users.length === 0){
+            res.status(404).send("No male user..")
+         }else{
+            res.send(users);
+         }
+         
+    }
+    catch(err){
+        res.status(400).send("Something went wrong" +err.message)
+    }
 });
+
+
+//Delete a user
+
+app.delete("/user",async (req,res)=>{
+    const userId = req.body.userId;
+
+    try{
+        const deleteduser = await User.findByIdAndDelete({userId}); 
+        res.send("User is deleted..")
+
+    }
+    catch(err){
+        res.status(404).send("User is not deleted .."+err);
+    }
+})
+
+//Update a User details
+
+app.patch("/user/:userId",async(req,res)=>{
+    const userId = req.params?.userId;
+    const data = req.body;
+    try{
+        const ALLOWED_UPDATES = ["age","city","phoneNumber"];
+        const isUpdateAllowed = Object.keys(data).every((k)=>
+            ALLOWED_UPDATES.includes(k)
+        );
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed");
+        }
+
+        const updatedUser = await User.findByIdAndUpdate({_id: userId}, data,{
+            runValidators: true,
+        });
+        res.send("User Updated successfully done..");
+
+    }
+    catch(err){
+        res.status(404).send("Something went wrong .."+err.message);
+    }
+})
 
 connectDB()
 .then(()=>{
@@ -32,9 +92,3 @@ connectDB()
 .catch((err)=>{
     console.log("Connection unsuccessfull!!" +err.message);
 });
-
-
-
-
-
-
--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,52 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const userSchema = new mongoose.Schema({
     firstName:{
         type : String,
+        required : true,
     },
     lastName:{
         type: String,
     },
+    emailId:{
+        type:String,
+        requires:true,
+        lowercase:true,
+        unique:true,
+        trim : true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error ("Enter valid email id - " + value);
+            }
+        }
+
+    },
     age:{
         type:Number,
+        required : true,
     },
     gender:{
         type:String,
+        validate(value){
+             if(!["male","female","other"].includes(value)){
+             throw new Error ("Gender is not valid");
+                }
+        },
     },
     city:{
-        type: String,
+        type: String,  
+    },
+    country:{
+        type : String,
+        default : "India",
     },
     phoneNumber:{
         type:Number,
+        required : true,
+        unique : true,
     },
+},{
+    timestamps : true,
 });
 
 module.exports = mongoose.model("User",userSchema);
